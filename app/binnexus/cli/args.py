15ed265f0d87395b008567ea43387e391db39613
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-21 06:51:02 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-21

--- app/binnexus/cli/args.py
+++ app/binnexus/cli/args.py
@@ -2,8 +2,8 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 20 апреля 2026 16:19:56
-# Version: 1.0.12
+# Last Updated: 21 апреля 2026 06:50:20
+# Version: 1.0.13
 # ========================================
 # app\binnexus\cli\args.py
 
