0b6c02175adb0c05cd4025286376a8daf4f67d37
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-30 09:18:24 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-30

--- app/binnexus/utils/logging.py
+++ app/binnexus/utils/logging.py
@@ -2,8 +2,8 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 29 апреля 2026 06:50:20
-# Version: 1.0.22
+# Last Updated: 30 апреля 2026 09:17:41
+# Version: 1.0.23
 # ========================================
 # app\binnexus\utils\logging.py
 
