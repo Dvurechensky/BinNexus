c187842cde7f3bc8d3c3f086e0d7acda990abb3b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-30 15:41:52 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-30

--- app/binnexus/portal/builder.py
+++ app/binnexus/portal/builder.py
@@ -2,8 +2,8 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 29 мая 2026 13:52:45
-# Version: 1.0.52
+# Last Updated: 30 мая 2026 15:41:06
+# Version: 1.0.53
 # ========================================
 # app\binnexus\portal\builder.py
 
