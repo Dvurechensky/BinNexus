4ec0266e049457601af1595f22200e3149652efc
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-29 06:51:12 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-29

--- app/binnexus/core/scanner.py
+++ app/binnexus/core/scanner.py
@@ -2,8 +2,8 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 28 июня 2026 11:40:27
-# Version: 1.0.82
+# Last Updated: 29 июня 2026 06:50:20
+# Version: 1.0.83
 # ========================================
 # app\binnexus\core\scanner.py
 
