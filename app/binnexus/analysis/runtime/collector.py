fd9a6ea94f0a4161675d06b59a623acd7c6d4db6
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-04 08:46:16 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-04

--- app/binnexus/analysis/runtime/collector.py
+++ app/binnexus/analysis/runtime/collector.py
@@ -2,8 +2,8 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 04 июля 2026 08:03:20
-# Version: 1.0.88
+# Last Updated: 04 июля 2026 08:45:44
+# Version: 1.0.89
 # ========================================
 # app\binnexus\analysis\runtime\collector.py
 
--- app/binnexus/core/__init__.py
+++ app/binnexus/core/__init__.py
@@ -2,6 +2,6 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 04 июля 2026 08:03:20
-# Version: 1.0.88
+# Last Updated: 04 июля 2026 08:45:44
+# Version: 1.0.89
 # ========================================
