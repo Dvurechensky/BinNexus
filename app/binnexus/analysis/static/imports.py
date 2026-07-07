7bc1cf4f48b41322538c4e3ee5adfed1831911e8
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-07 12:17:33 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-07

--- app/binnexus/analysis/static/imports.py
+++ app/binnexus/analysis/static/imports.py
@@ -2,8 +2,8 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 06 июля 2026 13:32:36
-# Version: 1.0.91
+# Last Updated: 07 июля 2026 12:16:47
+# Version: 1.0.92
 # ========================================
 # app\binnexus\analysis\static\imports.py
 
--- app/binnexus/export/__init__.py
+++ app/binnexus/export/__init__.py
@@ -2,6 +2,6 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 06 июля 2026 13:32:36
-# Version: 1.0.91
+# Last Updated: 07 июля 2026 12:16:47
+# Version: 1.0.92
 # ========================================
