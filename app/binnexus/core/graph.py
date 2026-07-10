f9b820efe0f450ea677483e3432b93f3e088b36f
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-10 11:53:15 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-10

--- app/binnexus/core/graph.py
+++ app/binnexus/core/graph.py
@@ -2,8 +2,8 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 09 июля 2026 06:59:21
-# Version: 1.0.94
+# Last Updated: 10 июля 2026 11:52:28
+# Version: 1.0.95
 # ========================================
 # app\binnexus\core\graph.py
 
--- app/binnexus/utils/__init__.py
+++ app/binnexus/utils/__init__.py
@@ -2,6 +2,6 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 09 июля 2026 06:59:21
-# Version: 1.0.94
+# Last Updated: 10 июля 2026 11:52:28
+# Version: 1.0.95
 # ========================================
