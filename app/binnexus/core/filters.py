b7827d4a9d7307a0775cf28119ea93f45bb59a7e
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-15 11:52:34 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-15

--- app/binnexus/core/filters.py
+++ app/binnexus/core/filters.py
@@ -2,8 +2,8 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 14 июля 2026 06:50:21
-# Version: 1.0.99
+# Last Updated: 15 июля 2026 11:51:43
+# Version: 1.0.100
 # ========================================
 # app\binnexus\core\filters.py 
 
