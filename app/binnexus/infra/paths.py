bb25fafe4102e2e2393ece9abf5f14fdd7e25e9d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-20 12:08:51 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-20

--- app/binnexus/infra/paths.py
+++ app/binnexus/infra/paths.py
@@ -2,8 +2,8 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 19 июля 2026 09:58:25
-# Version: 1.0.104
+# Last Updated: 20 июля 2026 12:08:07
+# Version: 1.0.105
 # ========================================
 # app\binnexus\infra\paths.py
 
--- app/binnexus/portal/__init__.py
+++ app/binnexus/portal/__init__.py
@@ -2,6 +2,6 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 19 июля 2026 09:58:25
-# Version: 1.0.104
+# Last Updated: 20 июля 2026 12:08:07
+# Version: 1.0.105
 # ========================================
