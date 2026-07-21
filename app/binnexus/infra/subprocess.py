bbff81ef94bca0d124b71ce85ef1c30f72cc7884
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-21 13:29:17 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-21

--- app/binnexus/infra/subprocess.py
+++ app/binnexus/infra/subprocess.py
@@ -2,8 +2,8 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 20 июля 2026 12:08:07
-# Version: 1.0.105
+# Last Updated: 21 июля 2026 13:28:29
+# Version: 1.0.106
 # ========================================
 # app\binnexus\infra\subprocess.py
 
--- app/binnexus/utils/__init__.py
+++ app/binnexus/utils/__init__.py
@@ -2,6 +2,6 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 20 июля 2026 12:08:07
-# Version: 1.0.105
+# Last Updated: 21 июля 2026 13:28:29
+# Version: 1.0.106
 # ========================================
