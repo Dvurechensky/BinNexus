ba9e04035cf689fb9e4fd8d45b4aed75bb5ab783
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-23 12:54:43 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-23

--- app/binnexus/core/models.py
+++ app/binnexus/core/models.py
@@ -2,8 +2,8 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 22 июля 2026 12:54:05
-# Version: 1.0.107
+# Last Updated: 23 июля 2026 12:53:59
+# Version: 1.0.108
 # ========================================
 # app\binnexus\core\models.py
 
