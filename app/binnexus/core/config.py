954a2501c636534d5ba153cb35b8d710e421a5a6
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-30 06:50:59 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-30

--- app/binnexus/core/config.py
+++ app/binnexus/core/config.py
@@ -2,8 +2,8 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 29 июля 2026 15:42:50
-# Version: 1.0.114
+# Last Updated: 30 июля 2026 06:50:21
+# Version: 1.0.115
 # ========================================
 # app\binnexus\core\config.py
 
--- app/binnexus/utils/__init__.py
+++ app/binnexus/utils/__init__.py
@@ -2,6 +2,6 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 29 июля 2026 15:42:50
-# Version: 1.0.114
+# Last Updated: 30 июля 2026 06:50:21
+# Version: 1.0.115
 # ========================================
