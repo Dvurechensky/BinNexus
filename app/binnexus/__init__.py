5ddcabcc1b5bf711b403aef23426f47cf7227688
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-26 06:51:08 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-26

--- app/binnexus/__init__.py
+++ app/binnexus/__init__.py
@@ -2,6 +2,6 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 25 августа 2026 06:50:21
-# Version: 1.0.141
+# Last Updated: 26 августа 2026 06:50:21
+# Version: 1.0.142
 # ========================================
--- app/binnexus/core/filters.py
+++ app/binnexus/core/filters.py
@@ -2,8 +2,8 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 25 августа 2026 06:50:21
-# Version: 1.0.141
+# Last Updated: 26 августа 2026 06:50:21
+# Version: 1.0.142
 # ========================================
 # app\binnexus\core\filters.py 
 
