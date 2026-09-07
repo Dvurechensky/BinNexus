37f7f398000c3e89e5c1cf22e72b11a1032dff01
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-07 08:14:49 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-07

--- app/binnexus/export/writer.py
+++ app/binnexus/export/writer.py
@@ -2,8 +2,8 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 06 сентября 2026 11:05:55
-# Version: 1.0.153
+# Last Updated: 07 сентября 2026 08:14:01
+# Version: 1.0.154
 # ========================================
 # app\binnexus\export\writer.py
 
